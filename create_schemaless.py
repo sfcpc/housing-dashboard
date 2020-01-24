df9987a008a5794c57a7e5a6c64f88dc8c24ab49
Steven Buss <sbuss@google.com>
2020-01-24 12:13:46 -0800

    Resolve parent<->child relationships

--- create_schemaless.py
+++ create_schemaless.py
@@ -11,6 +11,7 @@ from collections import defaultdict
 import csv
 from csv import DictReader
 from datetime import date
+from datetime import datetime
 import lzma
 import shutil
 import sys
@@ -19,11 +20,66 @@ import uuid
 csv.field_size_limit(sys.maxsize)
 
 
+def resolve_parent(record_id_metadata, record_id):
+    record = record_id_metadata[record_id]
+    if record['uuid']:
+        # Either implies this is a root or we have already resolved the parent
+        return record
+    if not record['parents']:
+        return record
+    all_parents = []
+    for idx, pid in enumerate(record['parents']):
+        if pid not in record_id_metadata:
+            # This implies this record is bad data and cannot be properly
+            # connected to a real parent record. 
+            continue
+        all_parents.append(resolve_parent(record_id_metadata, pid))
+    # If all_parents is empty, then none of the parents in record['parents']
+    # actually exist as valid records. So we can't link this to an exisitng
+    # uuid, so just return itself.
+    # # TODO: Come back to this when we are reading in DBI. At least a few
+    # of these records have related building permits.
+    if not all_parents:
+        return record
+    return sorted(all_parents,
+                  key=lambda x: x['date_opened'],
+                  reverse=True)[0]
+
+
 def just_dump(ppts_file, outfile):
     if ppts_file.endswith('.xz'):
         o = lzma.open
     else:
         o = open
+    record_id_metadata = {}
+    # This looks dumb, but the easiest way to ensure the parent->child mapping
+    # exists is to read through the file twice.
+    with o(ppts_file, mode='rt', encoding='utf-8', errors='replace') as inf:
+        reader = DictReader(inf)
+        for line in reader:
+            fk = line['record_id']
+            if line['parent']:
+                parents = line['parent'].split(',')
+            else:
+                parents = []
+            record_id_metadata[fk] = {
+                'uuid': None,
+                'date_opened': datetime.strptime(line['date_opened'].split(" ")[0], '%m/%d/%Y'),
+                'parents': parents,
+            }
+            if not parents:
+                record_id_metadata[fk]['uuid']: uuid.uuid4()
+
+    for fk, val in record_id_metadata.items():
+        if val['uuid']:
+            continue
+        puid = resolve_parent(record_id_metadata, fk)['uuid']
+        if not puid:
+            puid = uuid.uuid4()
+        record_id_metadata[fk]['uuid'] = puid
+    # At this point, all records have been associated with their parents
+
+
     with o(ppts_file, mode='rt', encoding='utf-8', errors='replace') as inf:
         reader = DictReader(inf)
         today = date.today()
