c8f8eb76f0bb6bd775e551864aef81630839d667
Steven Buss <sbuss@google.com>
2020-01-31 14:32:40 -0800

    Include MOHCD projects in testdata

--- gen-test-data.sh
+++ gen-test-data.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 xzcat data/ppts/2018-04-24-ppts.csv.xz | head -n1 > testdata/ppts-one.csv
-xzcat data/ppts/2018-04-24-ppts.csv.xz | egrep -i "(1950 mission)|(8 10th)|(429 beale)|(430 main)" >> testdata/ppts-one.csv
+xzcat data/ppts/2018-04-24-ppts.csv.xz | egrep -i "(1950 mission)|(8 10th)|(429 beale)|(430 main)|(2015-011205)" >> testdata/ppts-one.csv
 
 xzcat data/ppts/2019-06-02-ppts.csv.xz | head -n1 > testdata/ppts-two.csv
-xzcat data/ppts/2019-06-02-ppts.csv.xz | egrep -i "(1950 mission)|(8 10th)|(429 beale)|(430 main)" >> testdata/ppts-two.csv
+xzcat data/ppts/2019-06-02-ppts.csv.xz | egrep -i "(1950 mission)|(8 10th)|(429 beale)|(430 main)|(2015-011205)" >> testdata/ppts-two.csv
+
+cat data/mohcd/mohcd-2020-01-30.csv | head -n1 > testdata/mohcd.csv
+cat data/mohcd/mohcd-2020-01-30.csv | egrep -i "(1950 mission)|(2015-011205)|(*transbay)" >> testdata/mohcd.csv
+
 
 python3 -m schemaless.create_schemaless \
   --ppts_file testdata/ppts-one.csv \
   --tco_file testdata/tco.csv \
+  --mohcd_file testdata/mohcd.csv \
   --out_file testdata/schemaless-one.csv \
   --the-date=2020-01-29
 # We read in the uuid-map file generated previously so our uuids are stable
