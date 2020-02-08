5c92be6c9ae8688781a68c6942ed658a3fb14c49
Steven Buss <sbuss@google.com>
2020-02-07 17:41:26 -0800

    Add address_norm to Sources with address info

--- schemaless/sources.py
+++ schemaless/sources.py
@@ -166,6 +166,7 @@ class PPTS(Source):
         'date_closed': 'date_closed',
         # Location details
         'address': 'address',
+        'address_norm': Address('address'),
         'the_geom': 'the_geom',
 
         # Developer and Planner
@@ -263,6 +264,15 @@ class PTS(Source):
         'Unit': 'unit',
         'Unit Suffix': 'unit_suffix',
         'Zipcode': 'zipcode',
+        'address_norm': Address(
+            'street_number',
+            'street_number_suffix',
+            'street_name',
+            'street_name_suffix',
+            'unit',
+            'unit_suffix',
+            'zipcode',
+        ),
         'Location': 'location',
         'Supervisor District': 'supervisor_district',
         'Current Status': 'current_status',
@@ -293,6 +303,7 @@ class TCO(Source):
     FIELDS = {
         'Building Permit Application Number': 'building_permit_number',
         'Building Address': 'address',
+        'address_norm': Address('address'),
         'Date Issued': 'date_issued',
         'Document Type': 'building_permit_type',
         'Number of Units Certified': 'num_units',
@@ -356,7 +367,12 @@ class MOHCDPipeline(Source):
         'Street Name': 'street_name',
         'Street Type': 'street_type',
         'Zip Code': 'zip_code',
-        # TODO: How can we synthesize fields? eg add an 'address' field
+        'address_norm': Address(
+            'street_number'
+            'street_name',
+            'street_type',
+            'zip_code',
+        ),
         'Supervisor District': 'supervisor_district',
         'Location': 'location',  # This is a POINT()
         'Project Lead Sponsor': 'project_lead_sponsor',
@@ -440,6 +456,12 @@ class BMR(Source):
         'Street Name': 'street_name',
         'Street Type': 'street_type',
         'Zip Code': 'zip_code',
+        'address_norm': Address(
+            'street_number'
+            'street_name',
+            'street_type',
+            'zip_code',
+        ),
         'Location': 'location',
         'Supervisor District': 'supervisor_district',
         'Project Sponsor': 'project_sponsor',
