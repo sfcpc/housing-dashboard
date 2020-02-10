c62f9f4c997801f7f9c9565d07d89115217e0638
Steven Buss <sbuss@google.com>
2020-02-10 11:43:06 -0800

    Regenerate testdata with bmr data

--- gen-test-data.sh
+++ gen-test-data.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 xzcat data/ppts/2018-04-24-ppts.csv.xz | head -n1 > testdata/ppts-one.csv
-xzcat data/ppts/2018-04-24-ppts.csv.xz | egrep -i "(1950 mission)|(8 10th)|(429 beale)|(430 main)|(2015-011205)|(201404304554)|(201705237369)" >> testdata/ppts-one.csv
+xzcat data/ppts/2018-04-24-ppts.csv.xz | egrep -i "(1950 mission)|(8 10th)|(429 beale)|(430 main)|(2015-011205)|(201404304554)|(201705237369)|((340 Valencia)|(2002.0809))|(2451 Sacramento)" >> testdata/ppts-one.csv
 
 xzcat data/ppts/2019-06-02-ppts.csv.xz | head -n1 > testdata/ppts-two.csv
-xzcat data/ppts/2019-06-02-ppts.csv.xz | egrep -i "(1950 mission)|(8 10th)|(429 beale)|(430 main)|(2015-011205)|(201404304554)|(201705237369)" >> testdata/ppts-two.csv
+xzcat data/ppts/2019-06-02-ppts.csv.xz | egrep -i "(1950 mission)|(8 10th)|(429 beale)|(430 main)|(2015-011205)|(201404304554)|(201705237369)|((340 Valencia)|(2002.0809))|(2451 Sacramento)" >> testdata/ppts-two.csv
 
 head -n1 data/mohcd/mohcd-2020-01-30.csv > testdata/mohcd.csv
 cat data/mohcd/mohcd-2020-01-30.csv | egrep -i "(1950 mission)|(2015-011205)|(transbay)" >> testdata/mohcd.csv
 
+head -n1 data/mohcd/bmr-2019-09-06.csv > testdata/bmr.csv
+cat data/mohcd/bmr-2019-09-06.csv | egrep -i "(Valencia Gardens)|(Kennedy Towers)" >> testdata/bmr.csv
+
 xzcat data/pts/2020-01-24-pts-after-2013.csv.xz | head -n1 > testdata/pts.csv 
-xzcat data/pts/2020-01-24-pts-after-2013.csv.xz | egrep -i "(201705318009)|(201706058373)|(201711284964)|(201801118389)|(201404304554)|(201705237369)" >> testdata/pts.csv
+xzcat data/pts/2020-01-24-pts-after-2013.csv.xz | egrep -i "(201705318009)|(201706058373)|(201711284964)|(201801118389)|(201404304554)|(201705237369)|(2451,,Sacramento,St)" >> testdata/pts.csv
 
 python3 -m schemaless.create_schemaless \
   --ppts_file testdata/ppts-one.csv \
